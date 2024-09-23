befff62550eec71c35bb14455fd3c4b3986b2d17
amith-webdeveloper <amithwebdev1840@gmail.com>
2024-09-23 21:39:33 +0530

    revise basic js

---
+++ revise_js_concepts/script.js
@@ -0,0 +1,17 @@
+// javascript concepts
+
+// Arithmatic operator
+
+let x = 2;
+let y = 3;
+
+const sum = x + y;
+
+console.log(sum); // result 5  same as  [+ , - , * , /]
+
+let x1 = 10;
+let y1 = 11;
+
+const remainder = x1 % y1;
+
+console.log(remainder); // 

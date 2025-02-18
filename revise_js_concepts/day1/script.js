f605b3e65dd8b7e8f0f775d3bba593bc7c58c41c
amith-webdeveloper <amithwebdev1840@gmail.com>
2025-02-18 20:49:30 +0530

    c

--- revise_js_concepts/day1/script.js
+++ revise_js_concepts/day1/script.js
@@ -12,3 +12,93 @@
 // document.getElementById('myH1').textContent = `Hello`;
 // document.getElementById('myP').textContent = `I like Pizza`
 
+
+// //////////////////////////////Variables 
+
+// let x;
+// let y;
+
+// x = 100;
+
+// console.log(x);
+
+
+// let age = 25;
+// let price = 10.99;
+// // let gpa = 2.1;
+
+// console.log(age);
+// console.log(price);
+
+// console.log(typeof age);
+// console.log(typeof price);
+// console.log(typeof gpa);
+
+// console.log(`you are ${age} years old`);
+// console.log(`the price is ${price}`);
+// console.log(`your gpa is ${gpa}`);
+
+
+// let firstname = 'amith';
+// let favfood = 'pizza'
+// let email = 'amith@example.com'
+
+// console.log(typeof firstname);
+// console.log(firstname);
+
+// console.log(`hey my self ${firstname} and i like to eat ${favfood} and my email is ${email}`);
+
+// let online = true;
+// let forsale = false;
+
+// console.log(`bro is online :  ${online}`);
+// console.log(`the car is forsale : ${forsale}`);
+
+
+// let fullname = "amith"
+// let age = 25;
+// let isstudent = false
+
+// document.getElementById('p1').textContent = `Your name is ${fullname}`;
+// document.getElementById('p2').textContent = `Your are ${age} old`;
+// document.getElementById('p3').textContent = `Your are enrolled in school : ${isstudent}`;
+
+
+
+
+/////////////// arithematic operators
+
+let students = 30
+
+// students = students + 1
+// students = students - 1
+// students = students * 2
+// students = students / 2
+// students = students ** 2
+// extrastudents = students % 3
+
+// students += 1;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+// students **= 2;
+// students %= 2;
+
+// students++
+// students--
+
+// console.log(students);
+
+
+/*
+   operator precedence 
+   1. parenthesis () 
+   2. exponents
+   3. multiplication , division and modulo 
+   4. addition and substraction
+*/
+
+
+// Operands = (values , variables,  etc) 
+// operators = (+ , - , * , /)
+

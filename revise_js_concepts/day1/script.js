c2f88c1bd67614c05b949c80881d87a191967238
amith-webdeveloper <amithwebdev1840@gmail.com>
2025-02-18 20:31:54 +0530

    day 1 of js

---
+++ revise_js_concepts/day1/script.js
@@ -0,0 +1,14 @@
+
+// console.log(`Hello`);
+// console.log(`I like Pizza!`);
+
+// // window.alert(`This is an alert!`)
+// // window.alert(`I like pizza!`)
+
+// // This is a comment 
+
+// /* this is also comment */ 
+
+// document.getElementById('myH1').textContent = `Hello`;
+// document.getElementById('myP').textContent = `I like Pizza`
+

9fdb00b65ea55c5e360308745d9a57d3809b53ab
amith-webdeveloper <amithwebdev1840@gmail.com>
2025-02-19 20:59:43 +0530

    projects in js

---
+++ revise_js_concepts/day4/script.js
@@ -0,0 +1,38 @@
+
+// Math = built-in object that provides a collection of properties and methods
+
+
+console.log(Math.floor(Math.PI));
+
+let x = 3.21;
+let y = 2.01;
+let C = 2;
+let D = 3;
+let E = -23;
+let z;
+
+// z = Math.round(x)
+// console.log(z);
+
+
+// let A = Math.round(Math.PI)
+// console.log(A);
+
+
+// z = Math.floor(x)
+// z = Math.ceil(x) 
+// z = Math.trunc(x) remove decimal portion
+// z = Math.pow(C , D) // C to the power D 8
+// z = Math.sqrt(D)
+// z = Math.log(C) 
+// z = Math.sin(C)
+// z = Math.cos(C)
+// z = Math.tan(C)
+// z = Math.abs(E) // -23 converts to 23
+// z = Math.sign(E) // gives sign of that number in -1 or 1
+//  z = Math.max(x , y , C)  3.21
+//  z = Math.min(x , y , C)  2
+
+
+
+console.log(z);

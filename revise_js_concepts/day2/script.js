ade2b66904175940e81b63837e271c7baecc4e3c
amith-webdeveloper <amithwebdev1840@gmail.com>
2025-02-19 19:44:08 +0530

    day 2 js

---
+++ revise_js_concepts/day2/script.js
@@ -0,0 +1,20 @@
+// How to accept user input 
+
+// 1. Easy way = window prompt 
+// 2. professional way = html textbox
+
+// let username ;
+// username = prompt(`what is your name`)
+// console.log(username);
+
+// let username;
+
+
+// document.getElementById('mysubmit').onclick = function(){
+//     username = document.getElementById('mytext').value;
+//     console.log(username);
+//     document.getElementById('myh1').textContent = `Hello ${username}`
+    
+// }
+
+
